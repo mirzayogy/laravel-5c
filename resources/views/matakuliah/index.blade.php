fb700db5742736bd01a60961bba4ca6aabcbcaa1
mirzayogy <mirza.yogy@gmail.com>
2025-11-27 09:48:14 +0800

    perbaikan tampilan index

--- resources/views/matakuliah/index.blade.php
+++ resources/views/matakuliah/index.blade.php
@@ -1,6 +1,10 @@
 @extends('template.default')
 @section('content')
-    <h1 class="h3 mb-4 text-gray-800">Matakuliah</h1>
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Matakuliah</h1>
+        <a href="{{ route('matakuliah.create') }}" class="d-none d-sm-inline-block btn btn-sm btn-success shadow-sm">
+            <i class="fas fa-plus-circle fa-sm text-white-50"></i> Tambah</a>
+    </div>
     {{-- {{ dd($banyak_matakuliah) }} --}}
     @if (session('berhasil'))
         <br>
@@ -8,7 +12,9 @@
         <br>
     @endif
     <div class="card shadow mb-4">
-        <a href="{{ route('matakuliah.create') }}" class="btn btn-success">Tambah</a>
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Data Matakuliah</h6>
+        </div>
         <div class="card-body">
             <table class="table table-bordered">
                 <tr>
@@ -23,9 +29,20 @@
                         <td>{{ $matakuliah['kode_mk'] }}</td>
                         <td>{{ $matakuliah['nama'] }}</td>
                         <td>
-                            <a href="{{ route('matakuliah.edit', $matakuliah) }}" class="btn btn-primary">Ubah</a>
+                            <a href="{{ route('matakuliah.edit', $matakuliah) }}"
+                                class="btn btn-primary btn-sm btn-icon-split">
+                                {{-- <span class="icon text-white-50">
+                                    <i class="fas fa-edit"></i>
+                                </span> --}}
+                                <span class="text">Ubah</span>
+                            </a>
                             <a href="#" onclick="konfirmasi('{{ route('matakuliah.destroy', $matakuliah) }}')"
-                                class="btn btn-danger">Hapus</a>
+                                class="btn btn-danger btn-sm btn-icon-split">
+                                {{-- <span class="icon text-white-50">
+                                    <i class="fas fa-trash"></i>
+                                </span> --}}
+                                <span class="text">Hapus</span>
+                            </a>
                         </td>
                     </tr>
                 @endforeach
